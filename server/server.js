048452f60e0645f23217c71a0bb6adc3dd0183b2
Tshegofatso Mkhabela <tshego300@gmail.com>
2025-12-10 07:15:01 +0200

    fix: trailing / in link

--- server/server.js
+++ server/server.js
@@ -56,7 +56,7 @@ app.use(express.json({ limit: "4mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://trust-chat-mzansi.vercel.app/",
+    origin: "https://trust-chat-mzansi.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "token"],
