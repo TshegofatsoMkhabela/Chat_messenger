c138338155bec9c28fa7b21859809556b2ce7883
Tshegofatso Mkhabela <tshego300@gmail.com>
2025-12-10 08:08:47 +0200

    chore: nodemailer troubleshooting

--- server/services/emailService/config/smtpClientConfig.js
+++ server/services/emailService/config/smtpClientConfig.js
@@ -5,11 +5,14 @@ dotenv.config();
 
 // Create a reusable SMTP client
 export const smtpClient = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // true for 465, false for other ports
     auth: {
         user: process.env.GMAIL_USER,
         pass: process.env.GMAIL_APP_PASSWORD,
     },
+    connectionTimeout: 10000, // 10 seconds
 });
 
 // Optional: verify connection
